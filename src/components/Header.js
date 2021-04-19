b13f5608a0c2c83fb6557bcb7c65218cf2094873
winstonbarbe <winston.barbe@gmail.com>
2021-04-19 19:59:21 -0400

    inital commit and set up, with App, Header, and MemeGenerator components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header () {
+  return(
+    <header>
+      <img src="https://image.shutterstock.com/image-vector/cartoon-shouting-mouth-red-lipstick-260nw-120084256.jpg" alt="" />
+      <p>MemeGenerator</p>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MemeGenerator () {
+  return (
+    <div className="meme">
+      <h2>Meme Content</h2>
+    </div>
+  )
+}
+
+export default MemeGenerator
