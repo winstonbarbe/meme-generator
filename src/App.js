dfe19a98326404c648a7362e0c3d4c23a637ad2b
winstonbarbe <winston.barbe@gmail.com>
2021-04-19 22:19:12 -0400

    adds memes index component && deletes unused files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import MemesNew from './components/MemesNew'
+import MemesIndex from './components/MemesIndex'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MemesNew />
+      <MemesIndex />
     </div>
   );
 }
---
+++ src/components/MemesIndex.js
@@ -0,0 +1,9 @@
+function MemesIndex() {
+  return (
+    <div className="memes-index">
+      <h4>Howdy World</h4>
+    </div>
+  )
+}
+
+export default MemesIndex
