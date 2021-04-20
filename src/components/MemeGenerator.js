f232bb5540c538f1c3cd0455e684dbc92ae4f837
winstonbarbe <winston.barbe@gmail.com>
2021-04-19 20:09:11 -0400

    adds state to MemeGenerator component for meme info

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,9 +1,18 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
 
 function MemeGenerator () {
+  const [topText, setTopText] = useState("Top Meme Content");
+  const [bottomText, setBottomText] = useState("Bottom Meme Content");
+  const [randomImg, setRandomImg] = useState("http://i.imgflip.com/1bij.jpg")
+
+  
+
   return (
     <div className="meme">
-      <h2>Meme Content</h2>
+      <h2 className="top">{topText}</h2>
+      <h2 className="bottom">{bottomText}</h2>
+      <img src={randomImg} alt=""/>
     </div>
   )
 }
