6789bcbe5e0ff4d58ddd7c5df32698751e3442a6
winstonbarbe <winston.barbe@gmail.com>
2021-04-19 20:17:09 -0400

    implements a meme imgage API in the MemeGenerator file

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function MemeGenerator () {
   const [topText, setTopText] = useState("Top Meme Content");
   const [bottomText, setBottomText] = useState("Bottom Meme Content");
-  const [randomImg, setRandomImg] = useState("http://i.imgflip.com/1bij.jpg")
+  const [randomImg, setRandomImg] = useState("http://i.imgflip.com/1bij.jpg");
+  const [allMemeImgs, setAllMemeImgs] = useState([])
 
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        setAllMemeImgs(response.data.memes)
+      })
+  }, [])
   
 
   return (
