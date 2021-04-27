7d5a8062487b669352b3725812e4fc41282522d8
winstonbarbe <winston.barbe@gmail.com>
2021-04-26 21:40:39 -0400

    moves react around app element and shifts nav to header compoonent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
   Link
@@ -11,39 +10,18 @@ import MemesIndex from './components/MemesIndex'
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Router>
-        <div>
-          <nav>
-            <ul style={{listStyleType: "none", display: "flex", justifyContent: }}>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/memes-new">New Meme</Link>
-              </li>
-              <li>
-                <Link to="/memes-index">Memes Index</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-              renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/memes-new">
-              <MemesNew />
-            </Route>
-            <Route path="/memes-index">
-              <MemesIndex />
-            </Route>
-            <Route path="/">
-              
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-      
+        <Header />
+        <Switch>
+          <Route path="/memes-new">
+            <MemesNew />
+          </Route>
+          <Route path="/memes-index">
+            <MemesIndex />
+          </Route>
+          <Route path="/">
+            
+          </Route>
+        </Switch>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header () {
   return (
     <header>
       <img src="https://image.shutterstock.com/image-vector/cartoon-shouting-mouth-red-lipstick-260nw-120084256.jpg" alt="" />
       <p>MemeGenerator</p>
+      <nav className="header-nav">
+        <ul style={{listStyleType: "none", display: "flex", justifyContent: "space-around", color: "white"}}>
+          <li style={{color: "white"}}>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/memes-new">New Meme</Link>
+          </li>
+          <li>
+            <Link to="/memes-index">Memes Index</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
